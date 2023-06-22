b1e1dee3e727c7ab5495601a9e1b6d4186faad95
pttkachuk <petrotkachuk6@gmail.com>
2023-06-22 14:29:48 +0200

    fix pixabayApi.js

--- src/pixabayApi.js
+++ src/pixabayApi.js
@@ -4,7 +4,8 @@ async function fetchImages(value, page) {
     const BASE_URl = 'https://pixabay.com/api/';
     const API_KEY = '37626526-981480af389493ca84731da49';
     const filter = `?key=${API_KEY}&q=${value}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`;
-    return await axios.get(`${BASE_URl}${filter}`).then(responce => responce.data);
+    const responce = await axios.get(`${BASE_URl}${filter}`);
+    return responce.data;
 }
 
 export { fetchImages };
